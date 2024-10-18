215bb017eb4f5809e683c955e50926aed8750b5d
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-18 10:20:08 +0900

    Update and rename .gitignore to main.lua

---
+++ main.lua
@@ -0,0 +1,21 @@
+local teleportFunc = queueonteleport or queue_on_teleport
+    if teleportFunc then
+        teleportFunc([[
+            if not game:IsLoaded() then
+                game.Loaded:Wait()
+            end
+            repeat wait() until game.Players.LocalPlayer
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credit",Text = "By Nexer1234" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "alright dud"})
+wait(1)
+fireclickdetector(workspace.Orb.ClickDetector)
+wait(1)
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credit",Text = "Bind Glove Acquired" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "alright dud"})
+wait(1)
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credit",Text = "Getting Boxer + Fan" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "alright dud"})
+wait(1)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/sus/main/main.lua'))()
+]])
+end
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credit",Text = "By Nexer1234" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "alright dud"})
+wait(2)
+game:GetService("TeleportService"):Teleport(74169485398268)
